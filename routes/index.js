58e73618ea44fe71dbd7449d490fcbded4328a06
ketkub <ketwowwow@gmail.com>
2024-10-12 16:30:58 +0700

    T-T

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const postController = require('../controllers/postController');
-const restaurantController = require('../controllers/restaurantController');
-const imageController = require('../controllers/imageController');
 
 // User routes
 router.post('/users', userController.createUser);
 router.get('/users', userController.getUsers);
 router.get('/users/:id', userController.getUserById);
 router.put('/users/:id', userController.updateUser);
+
 router.delete('/users/:id', userController.deleteUser);
 
 // Post routes
@@ -19,19 +18,5 @@ router.get('/posts', postController.getPosts);
 router.get('/posts/:id', postController.getPostById);
 router.put('/posts/:id', postController.updatePost);
 router.delete('/posts/:id', postController.deletePost);
-
-// Restaurant routes
-router.post('/restaurants', restaurantController.createRestaurant);
-router.get('/restaurants', restaurantController.getRestaurants);
-router.get('/restaurants/:id', restaurantController.getRestaurantById);
-router.put('/restaurants/:id', restaurantController.updateRestaurant);
-router.delete('/restaurants/:id', restaurantController.deleteRestaurant);
-
-// Image routes (เชื่อมกับ restaurants)
-router.post('/restaurants/:restaurantId/images', imageController.createImage);
-router.get('/restaurants/:restaurantId/images', imageController.getRestaurantImages);
-router.get('/images/:id', imageController.getImageById);
-router.put('/images/:id', imageController.updateImage);
-router.delete('/images/:id', imageController.deleteImage);
 
 module.exports = router;
